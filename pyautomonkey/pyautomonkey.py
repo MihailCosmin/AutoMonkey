b356eefa683d199533e47c75cf78c561b0468eea
Mihail-Cosmin Munteanu <munteanu@althom.de>
2021-09-18 21:55:45 +0200

    Update pyautomonkey.py

--- pyautomonkey/pyautomonkey.py
+++ pyautomonkey/pyautomonkey.py
@@ -40,23 +40,56 @@ from cv2 import cv2
 from cv2 import imread
 from numpy import where
 
-def get_center(image: str):
-    """[summary]
+
+def add_ext(filename: str) -> str:
+    """Adds extension to an image filename if missing
+
     Args:
-        image ([str]): [image to be located]
+        filename (str): image filename (with or without extension)
+
     Returns:
-        [point]: [Returns the center of the located image as a point or None]
+        str: image filename with extension if was missing
     """
 
+    # Image Extensions supported
+    # TODO - Check if all work
     img_ext = (".png", ".jpg", ".jpeg", ".tiff", ".tif",
                ".bmp", ".gif", ".pdf", ".webp")
 
+    for ext in img_ext:
+        if isfile(f"{filename}{ext}"):
+            filename = f"{filename}{ext}"
+
+    return filename
+
+
+def is_on_screen(what: str) -> bool:
+    """Checks whether an image is found on screen
+    Args:
+        what (str): image to locate on screen
+    Returns:
+        bool: Returns True if image is found and False if not.
+    """
+
+    found = False
+    what = add_ext(what)
+
+    if locateOnScreen(what, confidence=0.9) is not None:
+        found = True
+
+    return found
+
+def get_center(image: str):
+    """Find the center of an image on screen
+    Args:
+        image ([str]): image to be located
+    Returns:
+        [point]: Returns the center of the located image as a point or None
+    """
+
+    image = add_ext(image)
+
     try:
-
-        for ext in img_ext:
-            if isfile(f"{image}{ext}"):
-                image = f"{image}{ext}"
-
         return center(locateOnScreen(image, confidence=0.9))
     except TypeError:
         return None
