9cb729cce577d3d5288efab970aa2c9f1356430a
Cosmin Munteanu <munteanu@althom.de>
2022-07-31 12:48:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = "-r requirements.txt"
+with open("requirements.txt", "r", encoding="utf-8") as _:
+    requirements = _.read().splitlines()
 
 test_requirements = ['pytest>=3', ]
 
