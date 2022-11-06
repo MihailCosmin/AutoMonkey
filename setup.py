f696cca3be4c01f81d4731ed979baa7ab55d8424
Mihail-Cosmin Munteanu <munteanu@althom.de>
2022-11-06 16:01:37 +0100

    final touches

--- setup.py
+++ setup.py
@@ -60,6 +60,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MihailCosmin/automonkey',
-    version='0.0.1',
+    version='0.0.2',
     zip_safe=False,
 )
