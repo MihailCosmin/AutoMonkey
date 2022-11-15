77e5a79b0c89bc6e673deb342b73e7cc8f981975
Mihail-Cosmin Munteanu <munteanu@althom.de>
2022-11-15 21:07:25 +0100

    PositionTracker

--- automonkey/automonkey.py
+++ automonkey/automonkey.py
@@ -314,8 +314,7 @@ class PositionTracker:  # was Toplevel
         self.window.canvas.configure(highlightthickness=0, bg='black')
         self.window.canvas.pack()
 
-        if self.window:
-            self.window.after(1, self._crosshair, None)
+        self.window.after(1, self._crosshair, None)
         self.window.mainloop()
         if get_coords:
             return self.coords
@@ -359,9 +358,12 @@ class PositionTracker:  # was Toplevel
                     fill='red',
                     font=("Helvetica", 40),
                 )
-        if self.window:
-            self.window.after(1, self._crosshair, coords)
 
+        self.window.after(1, self._crosshair, coords)
+
+    def destroy(self):
+        self.window.after_cancel(1)
+        self.window.destroy()
 
 def get_img_height(image_file: str) -> int:
     """Function that returns the height of an image.
