79816ea0bab69d9cdf01dc25c1db6966dbf61f3d
Cosmin Munteanu <munteanu@althom.de>
2025-05-08 16:28:19 +0000

    Bump version number

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Mihail-Cosmin Munteanu"""
 __email__ = 'munteanumihailcosmin@gmail.com'
-__version__ = '0.2.5'
+__version__ = '0.2.6'
--- automonkey/__init__.py
+++ automonkey/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Mihail-Cosmin Munteanu"""
 __email__ = 'munteanumihailcosmin@gmail.com'
-__version__ = '0.2.5'
+__version__ = '0.2.6'
 
 from .automonkey import *
